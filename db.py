a46b80534e2ed0cd66080784fa3eba2c6384e93c
55wind <52723688+55wind@users.noreply.github.com>
2023-12-29 16:42:57 +0900

    Update db.py

--- db.py
+++ db.py
@@ -28,6 +28,36 @@ class DbManager:
                           "REQUESTED integer default 0 not null);"
         self.c.execute(sql_create_blog)
 
+        #UserTable 생성
+        sql_user_table = """
+                    CREATE TABLE IF NOT EXISTS UserTable (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        naver_id TEXT NOT NULL,
+                        comment_count INTEGER DEFAULT 0 NOT NULL,
+                        like_count INTEGER DEFAULT 0 NOT NULL,
+                        neighbor_request_date DATE,
+                        created_date DATE DEFAULT CURRENT_DATE NOT NULL,
+                        updated_date DATE DEFAULT CURRENT_DATE NOT NULL
+                    );
+                """
+        self.c.execute(sql_user_table)
+
+        # UserPostTable 생성
+        sql_user_post_table = """
+                    CREATE TABLE IF NOT EXISTS UserPostTable (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER NOT NULL,
+                        post_name TEXT NOT NULL,
+                        post_body TEXT NOT NULL,
+                        written_comment TEXT,
+                        is_liked BOOLEAN,
+                        created_date DATE DEFAULT CURRENT_DATE NOT NULL,
+                        updated_date DATE DEFAULT CURRENT_DATE NOT NULL,
+                        FOREIGN KEY (user_id) REFERENCES UserTable(id)
+                    );
+                """
+        self.c.execute(sql_user_post_table)
+
     def create_user(self, username):
         sql_find_user = f'''SELECT * FROM blog where USERNAME=?'''
         self.c.execute(sql_find_user, (username, ))
@@ -60,6 +90,20 @@ class DbManager:
 
         return found_user[0]
 
+    #테이블 출력 함수
+    def list_tables(self):
+        sql_list_tables = "SELECT name FROM sqlite_master WHERE type='table';"
+        self.c.execute(sql_list_tables)
+        tables = self.c.fetchall()
+
+        if not tables:
+            print("Table does not exist")
+        else:
+            print("Table list:")
+            for table in tables:
+                print(table[0])
+
+
     def close(self):
         self.con.close()
 
@@ -67,5 +111,9 @@ class DbManager:
 db_manager = DbManager('./test.db')
 
 # db_manager.create_user('Jae')
+
+#테이블 리스트 출력
+db_manager.list_tables()
+
 
 print(db_manager.get_user('Jae'))
